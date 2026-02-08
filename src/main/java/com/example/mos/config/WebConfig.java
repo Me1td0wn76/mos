a3cd99025dccb6bded562e1ba93ab78bedfb16ff
Me1td0wn76 <jyoriku0103@gmail.com>
2026-02-08 19:13:46 +0900

    fix: Correct package name casing in WebConfig class

--- src/main/java/com/example/mos/config/WebConfig.java
+++ src/main/java/com/example/mos/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.mos.Config;
+package com.example.mos.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
